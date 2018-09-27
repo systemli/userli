b7ea5bfec07a577a0f6c6c4546874d6feb634dd7
louis <louis@systemli.org>
2018-09-27 16:19:32 +0200

    add testcase for PasswordGenerator

---
+++ tests/Helper/PasswordGeneratorTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Helper;
+
+use App\Helper\PasswordGenerator;
+use PHPUnit\Framework\TestCase;
+
+class PasswordGeneratorTest extends TestCase
+{
+    /**
+     * @dataProvider provider
+     *
+     * @param int $length
+     * @param int $iterations
+     */
+    public function testGenerate(int $length, int $iterations)
+    {
+        $password = PasswordGenerator::generate($length);
+
+        self::assertEquals($length, strlen($password));
+
+        for ($i = 0; $i <= $iterations; ++$i) {
+            self::assertNotEquals($password, PasswordGenerator::generate($length));
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function provider()
+    {
+        return [
+            [45, 1000],
+            [10, 100],
+        ];
+    }
+}
