e2bef06eeaa74f37dd2358ae7fac17d04bab1453
t2d <tim@systemli.org>
2018-09-28 11:39:22 +0200

    Test Entity/User

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use App\Enum\Roles;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class UserTest
+ * @package App\Tests\Entity
+ */
+class UserTest extends TestCase
+{
+
+    public function testGetRoles()
+    {
+        $user = new User();
+        $this->hasOnlyValidRoles($user->getRoles());
+
+        $user->setRoles([Roles::SUSPICIOUS]);
+        $this->hasOnlyValidRoles($user->getRoles());
+    }
+
+    /**
+     * @param $roles Roles[]
+     */
+    public function hasOnlyValidRoles($roles)
+    {
+        $this->assertInternalType('array', $roles);
+        foreach ($roles as $role) {
+            $this->assertContains($role, Roles::getAll());
+        }
+    }
+}
