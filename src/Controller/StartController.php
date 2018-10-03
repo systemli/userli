7c7b04cfe73624b60690a58c444873485d200de3
doobry <doobry@systemli.org>
2018-10-03 12:02:52 +0200

    Simplify StartController::indexAction, split `Start/index.html.twig`
    * Split `Start/index.html.twig` into separate templates:
      - `Start/vouchers.html.twig`
      - `Start/change_password.html.twig`
      - `Start/delete_account.html.twig`

--- src/Controller/StartController.php
+++ src/Controller/StartController.php
@@ -30,77 +30,77 @@ class StartController extends Controller
      */
     public function indexAction(Request $request)
     {
-        if ($this->isGranted('IS_AUTHENTICATED_FULLY')) {
-            /** @var User $user */
-            $user = $this->get('security.token_storage')->getToken()->getUser();
-            $voucherRepository = $this->get('doctrine')->getRepository('App:Voucher');
-
-            $codes = $voucherRepository->findOrCreateByUser($user);
-
-            $passwordChange = new PasswordChange();
-            $form = $this->createForm(
-                PasswordChangeType::class,
-                $passwordChange,
-                [
-                    'action' => $this->generateUrl('index'),
-                    'method' => 'post',
-                ]
-            );
-
-            $voucherForm = $this->createForm(
-                VoucherCreateType::class,
-                new VoucherCreate(),
-                [
-                    'action' => $this->generateUrl('index'),
-                    'method' => 'post',
-                ]
-            );
-
-            if ('POST' === $request->getMethod()) {
-                if ($request->request->has('create_voucher')) {
-                    $voucherForm->handleRequest($request);
-
-                    if ($voucherForm->isSubmitted() && $voucherForm->isValid()) {
-                        if ($this->isGranted('ROLE_SUPPORT')) {
-                            $voucher = $voucherRepository->createByUser($user);
-
-                            if ($voucher instanceof Voucher) {
-                                $request->getSession()->getFlashBag()->add('success', 'flashes.voucher-creation-successful');
-                            }
-                        }
-
-                        return $this->redirect($this->generateUrl('index'));
-                    }
-                } else {
-                    $form->handleRequest($request);
-
-                    if ($form->isSubmitted() && $form->isValid()) {
-                        $user->setPlainPassword($passwordChange->newPassword);
-
-                        $this->get(PasswordUpdater::class)->updatePassword($user);
-
-                        $this->getDoctrine()->getManager()->flush();
-
-                        $this->get('event_dispatcher')->dispatch(
-                            Events::MAIL_ACCOUNT_PASSWORD_CHANGED,
-                            new UserEvent($user)
-                        );
-
-                        $request->getSession()->getFlashBag()->add('success', 'flashes.password-change-successful');
-
-                        return $this->redirect($this->generateUrl('index'));
-                    }
-                }
-            }
-
-            return $this->render('Start/index.html.twig', array(
-                'user' => $user,
-                'codes' => $codes,
-                'form' => $form->createView(),
-                'voucher_form' => $voucherForm->createView(),
-            ));
-        } else {
+        if (!$this->isGranted('IS_AUTHENTICATED_FULLY')) {
             return $this->render('Start/index_anonymous.html.twig');
         }
+
+        /** @var User $user */
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $voucherRepository = $this->get('doctrine')->getRepository('App:Voucher');
+
+        $vouchers = $voucherRepository->findOrCreateByUser($user);
+
+        $passwordChange = new PasswordChange();
+        $form = $this->createForm(
+            PasswordChangeType::class,
+            $passwordChange,
+            [
+                'action' => $this->generateUrl('index'),
+                'method' => 'post',
+            ]
+        );
+
+        $voucherForm = $this->createForm(
+            VoucherCreateType::class,
+            new VoucherCreate(),
+            [
+                'action' => $this->generateUrl('index'),
+                'method' => 'post',
+            ]
+        );
+
+        if ('POST' === $request->getMethod()) {
+            if ($request->request->has('create_voucher')) {
+                $voucherForm->handleRequest($request);
+
+                if ($voucherForm->isSubmitted() && $voucherForm->isValid()) {
+                    if ($this->isGranted('ROLE_SUPPORT')) {
+                        $voucher = $voucherRepository->createByUser($user);
+
+                        if ($voucher instanceof Voucher) {
+                            $request->getSession()->getFlashBag()->add('success', 'flashes.voucher-creation-successful');
+                        }
+                    }
+
+                    return $this->redirect($this->generateUrl('index'));
+                }
+            } else {
+                $form->handleRequest($request);
+
+                if ($form->isSubmitted() && $form->isValid()) {
+                    $user->setPlainPassword($passwordChange->newPassword);
+
+                    $this->get(PasswordUpdater::class)->updatePassword($user);
+
+                    $this->getDoctrine()->getManager()->flush();
+
+                    $this->get('event_dispatcher')->dispatch(
+                        Events::MAIL_ACCOUNT_PASSWORD_CHANGED,
+                        new UserEvent($user)
+                    );
+
+                    $request->getSession()->getFlashBag()->add('success', 'flashes.password-change-successful');
+
+                    return $this->redirect($this->generateUrl('index'));
+                }
+            }
+        }
+
+        return $this->render('Start/index.html.twig', array(
+            'user' => $user,
+            'vouchers' => $vouchers,
+            'form' => $form->createView(),
+            'voucher_form' => $voucherForm->createView(),
+        ));
     }
 }
