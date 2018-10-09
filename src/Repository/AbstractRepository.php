f1a90caa482ccdbaac2205c0afa31363811e874f
doobry-systemli <doobry@systemli.org>
2018-10-09 14:07:46 +0200

    New class 'AbstractRepository' to filter out deleted entries (#25)
    * New class 'AbstractRepository' to filter out deleted entries
    
    * Create new Interface 'SoftDeletableInterface' to guarantee that
      'isDeleted()' exists
    
    * Fix phpdoc annotations for AbstractRepository
    
    * Fix return type in SoftDeletableInterface.php
    
    * Also enforce return type in method declaration
    
    * Set explicit type 'boo' for '$delete'
    
    * Revert change to use AbstractInterface in AliasInterface

---
+++ src/Repository/AbstractRepository.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\SoftDeletableInterface;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class AbstractRepository
+ */
+class AbstractRepository extends EntityRepository
+{
+    /**
+     * {@inheritdoc}
+     * @param bool $deleted
+     */
+    public function find($id, $lockMode = null, $lockVersion = null, bool $deleted = false)
+    {
+        $entity = parent::find($id, $lockMode, $lockVersion);
+        if ($entity instanceof SoftDeletableInterface) {
+            if (false === $deleted && $entity->isDeleted()) {
+                return null;
+            }
+        }
+
+        return $entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @param bool $deleted
+     */
+    public function findAll(bool $deleted = false)
+    {
+        return $this->findBy([], null, null, null, $deleted);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @param bool $deleted
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null, bool $deleted = false)
+    {
+        if (false === $deleted && $this->getClassMetadata()->hasField('deleted')) {
+            $criteria += ['deleted' => 0];
+        }
+
+        return parent::findBy($criteria, $orderBy, $limit, $offset);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @param bool $deleted
+     */
+    public function findOneBy(array $criteria, array $orderBy = null, bool $deleted = false)
+    {
+        if (false === $deleted && $this->getClassMetadata()->hasField('deleted')) {
+            $criteria += ['deleted' => 0];
+        }
+
+        return parent::findOneBy($criteria, $orderBy);
+    }
+}
