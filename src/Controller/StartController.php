bd4f19babf1757e2fa2ca4bb9b6739a35af22026
t2d <tim@systemli.org>
2018-10-23 15:08:57 +0200

    Add form for custom aliases

--- src/Controller/StartController.php
+++ src/Controller/StartController.php
@@ -9,6 +9,7 @@ use App\Exception\ValidationException;
 use App\Form\Model\AliasCreate;
 use App\Form\Model\PasswordChange;
 use App\Form\Model\VoucherCreate;
+use App\Form\CustomAliasCreateType;
 use App\Form\RandomAliasCreateType;
 use App\Form\PasswordChangeType;
 use App\Form\VoucherCreateType;
@@ -89,6 +90,16 @@ class StartController extends Controller
             ]
         );
 
+        $aliasCreate = new AliasCreate();
+        $customAliasCreateForm = $this->createForm(
+            CustomAliasCreateType::class,
+            $aliasCreate,
+            [
+                'action' => $this->generateUrl('index'),
+                'method' => 'post',
+            ]
+        );
+
         $passwordChange = new PasswordChange();
         $passwordChangeForm = $this->createForm(
             PasswordChangeType::class,
@@ -102,12 +113,15 @@ class StartController extends Controller
         if ('POST' === $request->getMethod()) {
             $voucherCreateForm->handleRequest($request);
             $randomAliasCreateForm->handleRequest($request);
+            $customAliasCreateForm->handleRequest($request);
             $passwordChangeForm->handleRequest($request);
 
             if ($voucherCreateForm->isSubmitted() && $voucherCreateForm->isValid()) {
                 $this->createVoucher($request, $user);
             } elseif ($randomAliasCreateForm->isSubmitted() && $randomAliasCreateForm->isValid()) {
                 $this->createRandomAlias($request, $user);
+            } elseif ($customAliasCreateForm->isSubmitted() && $customAliasCreateForm->isValid()) {
+                $this->createCustomAlias($request, $user, $aliasCreate->alias);
             } elseif ($passwordChangeForm->isSubmitted() && $passwordChangeForm->isValid()) {
                 $this->changePassword($request, $user, $passwordChange->newPassword);
             }
@@ -126,6 +140,7 @@ class StartController extends Controller
                 'vouchers' => $vouchers,
                 'voucher_form' => $voucherCreateForm->createView(),
                 'random_alias_form' => $randomAliasCreateForm->createView(),
+                'custom_alias_form' => $customAliasCreateForm->createView(),
                 'password_form' => $passwordChangeForm->createView(),
             ]
         );
@@ -160,6 +175,22 @@ class StartController extends Controller
             }
         } catch (ValidationException $e) {
             // Should not thrown
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @param User    $user
+     * @param string  $alias
+     */
+    private function createCustomAlias(Request $request, User $user, string $alias)
+    {
+        try {
+            if ($this->aliasHandler->create($user, $alias) instanceof Alias) {
+                $request->getSession()->getFlashBag()->add('success', 'flashes.custom-alias-creation-successful');
+            }
+        } catch (ValidationException $e) {
+            $request->getSession()->getFlashBag()->add('error', $e->getMessage());
         }
     }
 
---
+++ src/Form/CustomAliasCreateType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author tim <tim@systemli.org>
+ */
+class CustomAliasCreateType extends AbstractType
+{
+    const NAME = 'create_custom_alias';
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('alias', TextType::class, array('label' => 'form.new-custom-alias'))
+            ->add('submit', SubmitType::class, array('label' => 'form.create-custom-alias'));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(['data_class' => 'App\Form\Model\AliasCreate']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getBlockPrefix()
+    {
+        return self::NAME;
+    }
+}
