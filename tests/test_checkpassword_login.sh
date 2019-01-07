2ba5b5f0d1389864d1e6b1024d5c0c194ed1d975
t2d <tim@systemli.org>
2019-01-07 16:32:16 +0100

    Fix checkpassword test to use TLS

--- tests/test_checkpassword_login.sh
+++ tests/test_checkpassword_login.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-test=$((sleep 1; echo "USER admin@example.org"; sleep 1; echo "PASS password"; sleep 1) | telnet 192.168.33.99 110 2>&1 | grep "Logged in")
+test=$((sleep 1; echo "USER admin@example.org"; sleep 1; echo "PASS password"; sleep 1) | openssl s_client -connect 192.168.33.99:995 2>&1 | grep "Logged in")
 
 if [ -n "$test" ]; then
  echo OK
