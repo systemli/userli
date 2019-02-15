2d000152943987a35d4433be7a1f2cb6f60b9c6d
louis <louis@systemli.org>
2019-02-15 12:57:04 +0100

    rework flash notifications, fixed positioning and fadeout after 10 seconds

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,11 @@ require('../../vendor/twbs/bootstrap/dist/css/bootstrap.css');
 require('../../vendor/twbs/bootstrap/dist/js/bootstrap.min.js');
 require('../css/app.css');
 //require('../css/AdminLTE.css');
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+
+    setTimeout(function () {
+        $('.flash-notification').fadeOut();
+    }, 10000);
+});
