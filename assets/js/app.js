e81511d6f07f1e88f1f3ef28df46be988fc6fd68
louis <louis@systemli.org>
2019-02-19 21:12:26 +0100

    add copy to clipboard for voucher share links

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,25 @@ require('../../vendor/twbs/bootstrap/dist/css/bootstrap.css');
 require('../../vendor/twbs/bootstrap/dist/js/bootstrap.min.js');
 require('../css/app.css');
 //require('../css/AdminLTE.css');
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+
+    // We create a non visible textarea with the link, select them and tell the browser to copy the selected content
+    // after we copied the link the element will removed from DOM.
+    $('.invite-share-link').click(function (element) {
+        let el = document.createElement('textarea');
+
+        el.value = element.target.dataset.link;
+        el.setAttribute('readonly', '');
+        el.style.position = 'absolute';
+        el.style.left = '-9999px';
+        document.body.appendChild(el);
+        el.select();
+
+        document.execCommand('copy');
+
+        document.body.removeChild(el);
+    });
+});
