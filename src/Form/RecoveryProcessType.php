9c55534b815f96ea147cb4dc88e0e83fcb4b15b5
doobry <doobry@systemli.org>
2019-02-23 10:19:48 +0100

    Change recoveryToken input field to PasswordType in recovery form

--- src/Form/RecoveryProcessType.php
+++ src/Form/RecoveryProcessType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,7 +21,7 @@ class RecoveryProcessType extends AbstractType
     {
         $builder
             ->add('email', TextType::class, array('label' => 'form.email'))
-            ->add('recoveryToken', TextType::class, array('label' => 'form.recovery-token'))
+            ->add('recoveryToken', PasswordType::class, array('label' => 'form.recovery-token'))
             ->add('submit', SubmitType::class, array('label' => 'form.recovery-start'));
     }
 
