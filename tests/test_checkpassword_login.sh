9f0a52ec358d11c17aa226a0896b5d50a4f1a340
t2d <tim@systemli.org>
2019-06-20 19:19:32 +0200

    Make test_checkpassword_login.sh useful

--- tests/test_checkpassword_login.sh
+++ tests/test_checkpassword_login.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-test=$((sleep 1; echo "USER admin@example.org"; sleep 1; echo "PASS password"; sleep 1) | openssl s_client -connect 192.168.33.99:995 2>&1 | grep "Logged in")
-
-if [ -n "$test" ]; then
- echo OK
-else
- echo Fail
-fi
+openssl s_client -connect 192.168.33.99:995 -quiet <<EOF
+USER admin@example.org
+PASS password
+QUIT
+EOF
