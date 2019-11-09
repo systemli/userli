0765d92fd1d87c68d92bd6d181d01472f0446d17
Jeldrik Hanschke <admin@jhanschke.de>
2019-11-09 19:55:23 +0100

    refactor copy to clipboard button to make it more reusable
    Usage example:
    
      <button data-button="copy-to-clipboard" data-content="foo">
        copy foo to clipboard
      </button>
    
    This will copy "foo" to clipboard when user clicks the button.
    
    It's safe to use other elements inside without adding data-content
    to them as well. Event.currentTarget "always refers to the
    element to which the event handler has been attached, as opposed to
    Event.target, which identifies the element on which the event occurred
    and which may be its direct descendent." (MDN)

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,16 +3,24 @@ require('../../vendor/twbs/bootstrap/dist/js/bootstrap.min.js');
 require('../css/app.css');
 
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-    $('[data-toggle="popover"]').popover();
-
-    // We create a non visible textarea with the link,
-    // select them and tell the browser to copy the selected content
-    // after we copied the link the element will removed from DOM.
-    $('.invite-share-link').click(function (element) {
+    // Event handler that copies the value of element's [data-link]
+    // attribute to clipboard.
+    //
+    // Usage example:
+    //
+    //   <button data-value="foo">Copy foo to clickboard</button>
+    //
+    //   let el = document.querySelector('button');
+    //   el.addEventListener('click', copyToClipboard);
+    //
+    // To do so it creates a non visible textarea with the value,
+    // selects it and tell the browser to copy the selected content.
+    // After the value has been copied, the textarea is removed from
+    // DOM again.
+    function copyToClipboard(event) {
         let el = document.createElement('textarea');
 
-        el.value = element.target.dataset.link;
+        el.value = event.currentTarget.dataset.value;
         el.setAttribute('readonly', '');
         el.style.position = 'absolute';
         el.style.left = '-9999px';
@@ -22,7 +30,18 @@ $(function () {
         document.execCommand('copy');
 
         document.body.removeChild(el);
-    });
+    }
+
+    // initialize Bootstrap's tooltip and popover
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+
+    // initialize copy-to-clickboard buttons
+    document.querySelectorAll('[data-button="copy-to-clipboard"]')
+        .forEach(function(el) {
+            el.addEventListener('click', copyToClipboard);
+        });
+
     setTimeout(function () {
         $('.flash-notification').fadeOut();
     }, 10000);
