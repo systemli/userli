265c7c6e273988522814e21f3dc0712960270763
tim glitch <>
2021-12-25 14:52:31 +0100

    fix tests, which use the OpenPgpKey entity

--- src/Entity/OpenPgpKey.php
+++ src/Entity/OpenPgpKey.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Traits\CreationTimeTrait;
+use App\Traits\UpdatedTimeTrait;
 use App\Traits\EmailTrait;
 use App\Traits\IdTrait;
 use App\Traits\OpenPgpKeyTrait;
@@ -11,10 +12,21 @@ use App\Traits\UserAwareTrait;
 class OpenPgpKey
 {
     use CreationTimeTrait;
+    use UpdatedTimeTrait;
     use IdTrait;
     use UserAwareTrait;
     use EmailTrait;
     use OpenPgpKeyTrait;
+
+    /**
+     * OpenPgpKey constructor.
+     */
+    public function __construct()
+    {
+        $currentDateTime = new \DateTime();
+        $this->creationTime = $currentDateTime;
+        $this->updatedTime = $currentDateTime;
+    }
 
 
     public function toBinary(): ?string
