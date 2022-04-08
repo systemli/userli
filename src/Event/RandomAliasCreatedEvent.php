7e70ec2de04eee7665ef1ad9e0baf341074f2452
louis <louis@systemli.org>
2022-04-08 23:16:25 +0200

    :rotating_light: Fix Event deprecations

--- src/Event/RandomAliasCreatedEvent.php
+++ src/Event/RandomAliasCreatedEvent.php
@@ -4,7 +4,7 @@ namespace App\Event;
 
 use App\Entity\Alias;
 use App\Traits\AliasAwareTrait;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * Class RandomAliasCreatedEvent.
