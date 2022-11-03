742ae4569d1971fc956ce0c948a4a8e4cb11bac8
doobry <doobry@systemli.org>
2022-11-03 16:28:27 +0100

    Remove obsolete and unneeded symfony/web-server-bundle package

--- config/bundles.php
+++ config/bundles.php
@@ -20,5 +20,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
