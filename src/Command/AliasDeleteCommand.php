1f194a2f4e9731c0517b5d5d4d81d7d32a678dc3
doobry <doobry@systemli.org>
2023-11-07 23:23:17 +0100

    Add `app:alias:delete` command
    The command requires the alias source address as argument and supports
    an optional user email address as second argument. If the user email is
    provided, only a matching alias by the respective user is deleted.

---
+++ src/Command/AliasDeleteCommand.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\Alias;
+use App\Entity\User;
+use App\Handler\DeleteHandler;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+
+class AliasDeleteCommand extends Command
+{
+    private EntityManagerInterface $manager;
+    private DeleteHandler $deleteHandler;
+
+    public function __construct(EntityManagerInterface $manager, DeleteHandler $deleteHandler)
+    {
+        $this->manager = $manager;
+        $this->deleteHandler = $deleteHandler;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setName('app:alias:delete')
+            ->setDescription('Delete an alias')
+            ->addOption('user', 'u', InputOption::VALUE_REQUIRED, 'User who owns the alias (optional)')
+            ->addOption('alias', 'a', InputOption::VALUE_REQUIRED, 'Alias address to delete')
+            ->addOption('dry-run', null, InputOption::VALUE_NONE);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $email = $input->getOption('user');
+        $source = $input->getOption('alias');
+
+        $user = null;
+        if ($email && null === $user = $this->manager->getRepository(User::class)->findByEmail($email)) {
+            $output->writeln(sprintf("<error>User with email '%s' not found!</error>", $email));
+            return 1;
+        }
+
+        if (empty($source) || null === $alias = $this->manager->getRepository(Alias::class)->findOneBySource($source)) {
+            $output->writeln(sprintf("<error>Alias with address '%s' not found!</error>", $source));
+            return 1;
+        }
+
+        if ($input->getOption('dry-run')) {
+            if ($user) {
+                $output->write(sprintf("Would delete alias %s of user %s\n", $source, $email));
+            } else {
+                $output->write(sprintf("Would delete alias %s\n", $source));
+            }
+        } else {
+            if ($user) {
+                $output->write(sprintf("Deleting alias %s of user %s\n", $source, $email));
+                $this->deleteHandler->deleteAlias($alias, $user);
+            } else {
+                $output->write(sprintf("Deleting alias %s\n", $source));
+                $this->deleteHandler->deleteAlias($alias);
+            }
+        }
+
+        return 0;
+    }
+}
