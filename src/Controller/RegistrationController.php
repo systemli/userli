9b6b44886d26ce22c282527605e3e48f4f2f9127
doobry <doobry@systemli.org>
2024-03-22 16:54:24 +0100

    fix(registration): Fix order of registration routes
    Otherwise `/register/{voucher}` catches routing to
    `/register/recovery_token` and to `/register/welcome`

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -25,74 +25,6 @@ class RegistrationController extends AbstractController
 
     /**
      * @param Request $request
-     * @param string|null $voucher
-     * @return Response
-     * @throws Exception
-     */
-    #[Route(path: '/{_locale<%locales%>}/register', name: 'register')]
-    #[Route(path: '/{_locale<%locales%>}/register/{voucher}', name: 'register_voucher')]
-    public function register(Request $request, string $voucher = null): Response
-    {
-        if (!$this->registrationHandler->isRegistrationOpen()) {
-            return $this->render('Registration/closed.html.twig');
-        }
-
-        $registration = new Registration();
-        // Set voucher value in form if given as parameter in route
-        if (null !== $voucher) {
-            $registration->setVoucher($voucher);
-        }
-        $form = $this->createForm(
-            RegistrationType::class,
-            $registration,
-            [
-                'action' => $this->generateUrl('register'),
-                'method' => 'post',
-            ]
-        );
-
-        if ('POST' === $request->getMethod()) {
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                $this->registrationHandler->handle($registration);
-
-                if (null !== $user = $this->doctrine->getRepository(User::class)->findByEmail($registration->getEmail())) {
-                    $token = new UsernamePasswordToken($user, 'default', $user->getRoles());
-                    $this->tokenStorage->setToken($token);
-                }
-
-                $recoveryToken = $user->getPlainRecoveryToken();
-
-                // We have fetched plainRecoveryToken, which we need to show and can now remove
-                // all sensitive values from the user object
-                $user->eraseCredentials();
-
-                $recoveryTokenAck = new RecoveryTokenAck();
-                $recoveryTokenAck->setRecoveryToken($recoveryToken);
-                $recoveryTokenAckForm = $this->createForm(
-                    RecoveryTokenAckType::class,
-                    $recoveryTokenAck,
-                    [
-                        'action' => $this->generateUrl('register_recovery_token'),
-                        'method' => 'post',
-                    ]
-                );
-
-                return $this->render('Registration/recovery_token.html.twig',
-                    [
-                        'form' => $recoveryTokenAckForm->createView(),
-                        'recovery_token' => $recoveryToken,
-                    ]
-                );
-            }
-        }
-
-        return $this->render('Registration/register.html.twig', ['form' => $form->createView()]);
-    }
-
-    /**
-     * @param Request $request
      * @return Response
      */
     #[Route(path: '/{_locale<%locales%>}/register/recovery_token', name: 'register_recovery_token')]
@@ -133,4 +65,72 @@ class RegistrationController extends AbstractController
 
         return $this->render('Registration/welcome.html.twig');
     }
+
+	/**
+	 * @param Request $request
+	 * @param string|null $voucher
+	 * @return Response
+	 * @throws Exception
+	 */
+	#[Route(path: '/{_locale<%locales%>}/register', name: 'register')]
+	#[Route(path: '/{_locale<%locales%>}/register/{voucher}', name: 'register_voucher')]
+	public function register(Request $request, string $voucher = null): Response
+	{
+		if (!$this->registrationHandler->isRegistrationOpen()) {
+			return $this->render('Registration/closed.html.twig');
+		}
+
+		$registration = new Registration();
+		// Set voucher value in form if given as parameter in route
+		if (null !== $voucher) {
+			$registration->setVoucher($voucher);
+		}
+		$form = $this->createForm(
+			RegistrationType::class,
+			$registration,
+			[
+				'action' => $this->generateUrl('register'),
+				'method' => 'post',
+			]
+		);
+
+		if ('POST' === $request->getMethod()) {
+			$form->handleRequest($request);
+
+			if ($form->isSubmitted() && $form->isValid()) {
+				$this->registrationHandler->handle($registration);
+
+				if (null !== $user = $this->doctrine->getRepository(User::class)->findByEmail($registration->getEmail())) {
+					$token = new UsernamePasswordToken($user, 'default', $user->getRoles());
+					$this->tokenStorage->setToken($token);
+				}
+
+				$recoveryToken = $user->getPlainRecoveryToken();
+
+				// We have fetched plainRecoveryToken, which we need to show and can now remove
+				// all sensitive values from the user object
+				$user->eraseCredentials();
+
+				$recoveryTokenAck = new RecoveryTokenAck();
+				$recoveryTokenAck->setRecoveryToken($recoveryToken);
+				$recoveryTokenAckForm = $this->createForm(
+					RecoveryTokenAckType::class,
+					$recoveryTokenAck,
+					[
+						'action' => $this->generateUrl('register_recovery_token'),
+						'method' => 'post',
+					]
+				);
+
+				return $this->render('Registration/recovery_token.html.twig',
+					[
+						'form' => $recoveryTokenAckForm->createView(),
+						'recovery_token' => $recoveryToken,
+					]
+				);
+			}
+		}
+
+		return $this->render('Registration/register.html.twig', ['form' => $form->createView()]);
+	}
 }
