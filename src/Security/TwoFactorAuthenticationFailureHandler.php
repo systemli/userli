f0d598c70f8e847e789e300fc9da99fe17bf95b6
yena <yena@systemli.org>
2024-03-28 18:11:24 +0100

    [WIP] login api
    Adds LexikJWT auth bundle and JSON login

---
+++ src/Security/TwoFactorAuthenticationFailureHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+
+class TwoFactorAuthenticationFailureHandler implements AuthenticationFailureHandlerInterface
+{
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        return new Response('{"error": "2fa_failed", "two_factor_complete": false}');
+   }
+}
---
+++ src/Security/TwoFactorAuthenticationSuccessHandler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationSuccessHandler; 
+
+class TwoFactorAuthenticationSuccessHandler extends AuthenticationSuccessHandler
+{
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): Response
+    {
+        return $this->handleAuthenticationSuccess($token->getUser());   
+    }
+}
