124bf8ba9e90073c86c422ee8bd8d268fa3494bc
0x46616c6b <0x46616c6b@users.noreply.github.com>
2024-11-09 16:20:10 +0100

    Upgrade Symfony to 6.4.14 (#659)
    * Upgrade Symfony to 6.4.14
    
    * Upgrade nelmio/security-bundle recipe
    
    * Upgrade symfony/console recipe
    
    * Upgrade symfony/mailer recipe
    
    * Remove explicit twig dependency
    
    * Fix tests

--- config/bundles.php
+++ config/bundles.php
@@ -19,6 +19,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
