6c9165a130a9a9ec8e923e59c888fcb2414e3838
doobry <doobry@systemli.org>
2025-04-13 14:55:17 +0200

    Add tests for recovery token in UsersRestoreCommand

--- tests/Command/UsersRestoreCommandTest.php
+++ tests/Command/UsersRestoreCommandTest.php
@@ -6,9 +6,6 @@ use App\Command\UsersRestoreCommand;
 use App\Handler\UserRestoreHandler;
 use Exception;
 use App\Entity\User;
-use App\Handler\MailCryptKeyHandler;
-use App\Handler\RecoveryTokenHandler;
-use App\Helper\PasswordUpdater;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use PHPUnit\Framework\TestCase;
@@ -19,6 +16,7 @@ use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 class UsersRestoreCommandTest extends TestCase
 {
     private UsersRestoreCommand $command;
+    private UserRestoreHandler $userRestoreHandler;
     private User $user;
 
     public function setUp(): void
@@ -38,19 +36,20 @@ class UsersRestoreCommandTest extends TestCase
             ->getMock();
         $manager->method('getRepository')->willReturn($repository);
 
-        $userRestoreHandler = $this->getMockBuilder(UserRestoreHandler::class)
+        $this->userRestoreHandler = $this->getMockBuilder(UserRestoreHandler::class)
             ->disableOriginalConstructor()
             ->getMock();
-        $userRestoreHandler->method('restoreUser')->willReturnCallback(function (User $user, string $password) {
-           $user->setDeleted(false);
-           $user->setMailCryptEnabled(true);
-        });
 
-        $this->command = new UsersRestoreCommand($manager, $userRestoreHandler);
+        $this->command = new UsersRestoreCommand($manager, $this->userRestoreHandler);
     }
 
-    public function testExecute(): void
+    public function testExecuteWithoutMailCrypt(): void
     {
+        $this->userRestoreHandler->method('restoreUser')->willReturnCallback(function (User $user, string $password) {
+            $user->setDeleted(false);
+            return null;
+        });
+
         $application = new Application();
         $application->add($this->command);
 
@@ -68,12 +67,41 @@ class UsersRestoreCommandTest extends TestCase
         // Test real run
         $commandTester->execute(['--user' => 'deleted@example.org']);
 
-        // Verify that user properties got restore
+        // Verify that user properties got restored
+        self::assertFalse($this->user->isDeleted());
+        self::assertFalse($this->user->getMailCryptEnabled());
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Restoring user deleted@example.org', $output);
+        $this->assertStringNotContainsString('New recovery token (please hand over to user): ', $output);
+    }
+
+    public function testExecuteWithMailCrypt(): void
+    {
+        $this->userRestoreHandler->method('restoreUser')->willReturnCallback(function (User $user, string $password) {
+            $user->setDeleted(false);
+            $user->setMailCryptEnabled(true);
+            return 'RecoverySecret';
+        });
+
+        $application = new Application();
+        $application->add($this->command);
+
+        $command = $application->find('app:users:restore');
+
+        $commandTester = new CommandTester($command);
+        $commandTester->setInputs(['longtestpassword1234', 'longtestpassword1234']);
+
+        // Test real run
+        $commandTester->execute(['--user' => 'deleted@example.org']);
+
+        // Verify that user properties got restored
         self::assertFalse($this->user->isDeleted());
         self::assertTrue($this->user->getMailCryptEnabled());
 
         $output = $commandTester->getDisplay();
         $this->assertStringContainsString('Restoring user deleted@example.org', $output);
+        $this->assertStringContainsString('New recovery token (please hand over to user): RecoverySecret', $output);
     }
 
     public function testExecuteShortPassword(): void
