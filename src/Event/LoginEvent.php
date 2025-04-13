491a400d8f5801e5e764d2bf5769cf66f0f7b755
yena <yena@systemli.org>
2025-04-13 02:28:40 +0200

    Refactor MailCrypt key creation on login

--- src/Event/LoginEvent.php
+++ src/Event/LoginEvent.php
@@ -3,23 +3,22 @@
 namespace App\Event;
 
 use App\Entity\User;
+use App\Traits\PlainPasswordTrait;
 use App\Traits\UserAwareTrait;
 use Symfony\Contracts\EventDispatcher\Event;
 
-/**
- * Class LoginEvent.
- */
 class LoginEvent extends Event
 {
     use UserAwareTrait;
+    use PlainPasswordTrait;
 
     public const NAME = 'user.login';
 
-    /**
-     * Constructor.
-     */
-    public function __construct(User $user)
+
+
+    public function __construct(User $user, string $plainPassword)
     {
         $this->user = $user;
+        $this->plainPassword = $plainPassword;
     }
 }
