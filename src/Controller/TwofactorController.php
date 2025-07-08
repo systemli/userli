bcfa739a65e0f0621c12d2010ccfc0dbb8f503c6
louis <louis@systemli.org>
2025-07-08 22:47:06 +0200

    ♻️Refactor Twofactor Controller and Template

--- src/Controller/TwofactorController.php
+++ src/Controller/TwofactorController.php
@@ -29,279 +29,149 @@ class TwofactorController extends AbstractController
     {
     }
 
-    /**
-     * @param Request $request
-     * @param TotpAuthenticatorInterface $totpAuthenticator
-     * @return Response
-     * @throws RuntimeException
-     */
-    #[Route(path: '/user/twofactor', name: 'user_twofactor')]
-    public function twofactor(Request $request, TotpAuthenticatorInterface $totpAuthenticator): Response
+    #[Route(path: '/user/twofactor', name: 'user_twofactor', methods: ['GET'])]
+    public function show(): Response
     {
-        /** @var $user TwoFactorInterface */
-        if (null === $user = $this->getUser()) {
-            throw new RuntimeException('User should not be null');
+        if (!$this->getUser()->isTotpAuthenticationEnabled()) {
+            $form = $this->createForm(TwofactorType::class, new Twofactor(), [
+                'action' => $this->generateUrl('user_twofactor_submit'),
+                'method' => 'POST',
+            ]);
+            return $this->render('User/twofactor_enable.html.twig', [
+                'form' => $form->createView(),
+                'twofactor_enabled' => false,
+            ]);
         }
 
+        $form = $this->createForm(TwofactorType::class, new Twofactor(), [
+            'action' => $this->generateUrl('user_twofactor_disable'),
+            'method' => 'POST',
+        ]);
+
+        return $this->render('User/twofactor_disable.html.twig', [
+            'form' => $form->createView(),
+            'twofactor_enabled' => true,
+        ]);
+    }
+
+    #[Route(path: '/user/twofactor', name: 'user_twofactor_submit', methods: ['POST'])]
+    public function submit(Request $request, TotpAuthenticatorInterface $totpAuthenticator): Response
+    {
+        $user = $this->getUser();
         $form = $this->createForm(TwofactorType::class, new Twofactor());
+        $form->handleRequest($request);
 
-        $confirmForm = $this->createForm(
-            TwofactorConfirmType::class,
-            new TwofactorConfirm(),
-            [
-                'action' => $this->generateUrl('user_twofactor_confirm'),
-                'method' => 'post',
-            ]
-        );
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setTotpSecret($totpAuthenticator->generateSecret());
+            $user->generateBackupCodes();
+            $this->manager->flush();
 
-        $disableForm = $this->createForm(
-            TwofactorType::class,
-            new Twofactor(),
-            [
-                'action' => $this->generateUrl('user_twofactor_disable'),
-                'method' => 'post',
-            ]
-        );
-
-        if ('POST' === $request->getMethod()) {
-            $form->handleRequest($request);
-            $disableForm->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                $user->setTotpSecret($totpAuthenticator->generateSecret());
-                $user->generateBackupCodes();
-                $this->manager->flush();
-
-                return $this->render('User/twofactor.html.twig',
-                    [
-                        'form' => $form->createView(),
-                        'confirmForm' => $confirmForm->createView(),
-                        'disableForm' => $disableForm->createView(),
-                        'twofactor_enable' => true,
-                        'twofactor_enabled' => $user->isTotpAuthenticationEnabled(),
-                    ]
-                );
-            }
+            return $this->redirectToRoute('user_twofactor_confirm');
         }
 
-        return $this->render('User/twofactor.html.twig',
+        return $this->render('User/twofactor_enable.html.twig', [
+            'form' => $form->createView(),
+            'twofactor_enabled' => false,
+        ]);
+    }
+
+    #[Route(path: '/user/twofactor_confirm', name: 'user_twofactor_confirm', methods: ['GET'])]
+    public function confirm(): Response
+    {
+        $form = $this->createForm(TwofactorConfirmType::class, new TwofactorConfirm(), [
+            'action' => $this->generateUrl('user_twofactor_confirm_submit'),
+            'method' => 'POST',
+        ]);
+
+        return $this->render('User/twofactor_confirm.html.twig',
             [
                 'form' => $form->createView(),
-                'confirmForm' => $confirmForm->createView(),
-                'disableForm' => $disableForm->createView(),
-                'twofactor_enable' => false,
+                'twofactor_enabled' => $this->getUser()->isTotpAuthenticationEnabled(),
+            ]
+        );
+    }
+
+    #[Route(path: '/user/twofactor_confirm', name: 'user_twofactor_confirm_submit', methods: ['POST'])]
+    public function confirmSubmit(Request $request): Response
+    {
+        $form = $this->createForm(TwofactorConfirmType::class, new TwofactorConfirm());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('user_twofactor_backup_ack');
+        }
+
+        return $this->render('User/twofactor_confirm.html.twig',
+            [
+                'form' => $form->createView(),
+                'twofactor_enabled' => $this->getUser()->isTotpAuthenticationEnabled(),
+            ]
+        );
+    }
+
+    #[Route(path: '/user/twofactor_backup_codes', name: 'user_twofactor_backup_ack', methods: ['GET'])]
+    public function backupAck(): Response
+    {
+        $form = $this->createForm(TwofactorBackupAckType::class, new TwofactorBackupAck(), [
+            'action' => $this->generateUrl('user_twofactor_backup_ack_submit'),
+            'method' => 'POST',
+        ]);
+
+        return $this->render('User/twofactor_backup_ack.html.twig',
+            [
+                'form' => $form->createView(),
+                'twofactor_enabled' => $this->getUser()->isTotpAuthenticationEnabled(),
+                'twofactor_backup_codes' => $this->getUser()->getBackupCodes(),
+            ]
+        );
+    }
+
+    #[Route(path: '/user/twofactor_backup_codes', name: 'user_twofactor_backup_ack_submit', methods: ['POST'])]
+    public function backupAckSubmit(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(TwofactorBackupAckType::class, new TwofactorBackupAck());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setTotpConfirmed(true);
+            $this->manager->flush();
+
+            return $this->redirectToRoute('user_twofactor');
+        }
+
+        return $this->render('User/twofactor_backup_ack.html.twig',
+            [
+                'form' => $form->createView(),
+                'twofactor_enabled' => $user->isTotpAuthenticationEnabled(),
+                'twofactor_backup_codes' => $user->getBackupCodes(),
+            ]
+        );
+    }
+
+    #[Route(path: '/user/twofactor_disable', name: 'user_twofactor_disable', methods: ['POST'])]
+    public function disable(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(TwofactorType::class, new Twofactor());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setTotpConfirmed(false);
+            $user->setTotpSecret(null);
+            $this->manager->flush();
+
+            return $this->redirectToRoute('user_twofactor');
+        }
+
+        return $this->render('User/twofactor_disable.html.twig',
+            [
+                'form' => $form->createView(),
                 'twofactor_enabled' => $user->isTotpAuthenticationEnabled(),
             ]
         );
     }
 
-    /**
-     * @param Request $request
-     * @return Response
-     * @throws RuntimeException
-     */
-    #[Route(path: '/user/twofactor_confirm', name: 'user_twofactor_confirm')]
-    public function twofactorConfirm(Request $request): Response
-    {
-        if (null === $user = $this->getUser()) {
-            throw new RuntimeException('User should not be null');
-        }
-
-        $confirmForm = $this->createForm(TwofactorConfirmType::class, new TwofactorConfirm());
-
-        if ('POST' === $request->getMethod()) {
-            $confirmForm->handleRequest($request);
-
-            $form = $this->createForm(
-                TwofactorType::class,
-                new Twofactor(),
-                [
-                    'action' => $this->generateUrl('user_twofactor'),
-                    'method' => 'post',
-                ]
-            );
-
-            $disableForm = $this->createForm(
-                TwofactorType::class,
-                new Twofactor(),
-                [
-                    'action' => $this->generateUrl('user_twofactor_disable'),
-                    'method' => 'post',
-                ]
-            );
-
-            $backupAckForm = $this->createForm(
-                TwofactorBackupAckType::class,
-                new TwofactorBackupAck(),
-                [
-                    'action' => $this->generateUrl('user_twofactor_backup_ack'),
-                    'method' => 'post',
-                ]
-            );
-
-            if ($confirmForm->isSubmitted()) {
-                if ($confirmForm->isValid()) {
-                    return $this->render('User/twofactor.html.twig',
-                        [
-                            'form' => $form->createView(),
-                            'confirmForm' => $confirmForm->createView(),
-                            'backupAckForm' => $backupAckForm->createView(),
-                            'disableForm' => $disableForm->createView(),
-                            'twofactor_enable' => true,
-                            'twofactor_enabled' => $user->isTotpAuthenticationEnabled(),
-                            'twofactor_backup_codes' => $user->getBackupCodes(),
-                        ]
-                    );
-                }
-
-                // Again render form to display form errors
-                return $this->render('User/twofactor.html.twig',
-                    [
-                        'form' => $form->createView(),
-                        'confirmForm' => $confirmForm->createView(),
-                        'backupAckForm' => $backupAckForm->createView(),
-                        'disableForm' => $disableForm->createView(),
-                        'twofactor_enable' => true,
-                        'twofactor_enabled' => $user->isTotpAuthenticationEnabled(),
-                    ]
-                );
-            }
-        }
-
-        return $this->redirectToRoute('user_twofactor');
-    }
-
-    /**
-     * @param Request $request
-     * @return Response
-     * @throws RuntimeException
-     */
-    #[Route(path: '/user/twofactor_backup_codes', name: 'user_twofactor_backup_ack')]
-    public function twofactorBackupAck(Request $request): Response
-    {
-        if (null === $user = $this->getUser()) {
-            throw new RuntimeException('User should not be null');
-        }
-
-        $backupAckForm = $this->createForm(
-            TwofactorBackupAckType::class,
-            new TwofactorBackupAck(),
-            [
-                'action' => $this->generateUrl('user_twofactor_backup_ack'),
-                'method' => 'post',
-            ]
-        );
-
-        if ('POST' === $request->getMethod()) {
-            $backupAckForm->handleRequest($request);
-
-            if ($backupAckForm->isSubmitted()) {
-                if ($backupAckForm->isValid()) {
-                    $user->setTotpConfirmed(true);
-                    $this->manager->flush();
-
-                    return $this->redirectToRoute('user_twofactor');
-                } else {
-                    $form = $this->createForm(
-                        TwofactorType::class,
-                        new Twofactor(),
-                        [
-                            'action' => $this->generateUrl('user_twofactor'),
-                            'method' => 'post',
-                        ]
-                    );
-
-                    $confirmForm = $this->createForm(
-                        TwofactorConfirmType::class,
-                        new TwofactorConfirm(),
-                        [
-                            'action' => $this->generateUrl('user_twofactor_confirm'),
-                            'method' => 'post',
-                        ]
-                    );
-
-                    $disableForm = $this->createForm(
-                        TwofactorType::class,
-                        new Twofactor(),
-                        [
-                            'action' => $this->generateUrl('user_twofactor_disable'),
-                            'method' => 'post',
-                        ]
-                    );
-
-                    return $this->render('User/twofactor.html.twig',
-                        [
-                            'form' => $form->createView(),
-                            'confirmForm' => $confirmForm->createView(),
-                            'backupAckForm' => $backupAckForm->createView(),
-                            'disableForm' => $disableForm->createView(),
-                            'twofactor_enable' => true,
-                            'twofactor_enabled' => $user->isTotpAuthenticationEnabled(),
-                            'twofactor_backup_codes' => $user->getBackupCodes(),
-                        ]
-                    );
-                }
-            }
-        }
-
-        return $this->redirectToRoute('user_twofactor');
-    }
-
-    /**
-     * @param Request $request
-     * @return Response
-     * @throws RuntimeException
-     */
-    #[Route(path: '/user/twofactor_disable', name: 'user_twofactor_disable')]
-    public function twofactorDisable(Request $request): Response
-    {
-        if (null === $user = $this->getUser()) {
-            throw new RuntimeException('User should not be null');
-        }
-
-        $disableForm = $this->createForm(TwofactorType::class, new Twofactor());
-
-        if ('POST' === $request->getMethod()) {
-            $disableForm->handleRequest($request);
-
-            if ($disableForm->isSubmitted()) {
-                if ($disableForm->isValid()) {
-                    $user->setTotpConfirmed(false);
-                    $user->setTotpSecret(null);
-                    $this->manager->flush();
-
-                    return $this->redirectToRoute('user_twofactor');
-                }
-
-                $form = $this->createForm(
-                    TwofactorType::class,
-                    new Twofactor(),
-                    [
-                        'action' => $this->generateUrl('user_twofactor'),
-                        'method' => 'post',
-                    ]
-                );
-
-                // Again render form to display form errors
-                return $this->render('User/twofactor.html.twig',
-                    [
-                        'form' => $form->createView(),
-                        'disableForm' => $disableForm->createView(),
-                        'twofactor_enable' => false,
-                        'twofactor_enabled' => $user->isTotpAuthenticationEnabled(),
-                    ]
-                );
-            }
-        }
-
-        return $this->redirectToRoute('user_twofactor');
-    }
-
-    /**
-     * @param TotpAuthenticatorInterface $totpAuthenticator
-     * @return Response
-     * @throws RuntimeException
-     */
     #[Route(path: '/user/twofactor/qrcode', name: 'user_twofactor_qrcode')]
     public function displayTotpQrCode(TotpAuthenticatorInterface $totpAuthenticator): Response
     {
