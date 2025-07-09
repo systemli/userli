f129453ece99f066ef461992a45ef350159e05d0
0x46616c6b <0x46616c6b@users.noreply.github.com>
2025-07-09 20:46:15 +0200

    ♻️ Refactor InitController (#801)
    * ♻️ Refactor InitController
    
    * Add more tests

--- src/Controller/InitController.php
+++ src/Controller/InitController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Creator\DomainCreator;
 use App\Entity\Domain;
 use App\Entity\User;
-use App\Exception\ValidationException;
 use App\Form\DomainCreateType;
 use App\Form\Model\DomainCreate;
 use App\Form\Model\PlainPassword;
@@ -19,51 +18,46 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class InitController extends AbstractController
 {
-    public function __construct(private readonly EntityManagerInterface $manager, private readonly AdminPasswordUpdater $updater, private readonly DomainCreator $creator)
+    public function __construct(
+        private readonly EntityManagerInterface $manager,
+        private readonly AdminPasswordUpdater   $updater,
+        private readonly DomainCreator          $creator
+    )
     {
     }
 
-    /**
-     * @param Request $request
-     * @return Response
-     * @throws ValidationException
-     */
-    #[Route(path: '/init', name: 'init')]
-    public function index(Request $request): Response
+    #[Route(path: '/init', name: 'init', methods: ['GET'])]
+    public function init(Request $request): Response
     {
         // redirect if already configured
         if (0 < $this->manager->getRepository(Domain::class)->count([])) {
             return $this->redirectToRoute('init_user');
         }
 
-        $domain = new DomainCreate();
-        $domainForm = $this->createForm(
-            DomainCreateType::class,
-            $domain,
-            [
-                'action' => $this->generateUrl('init'),
-                'method' => 'post',
-            ]
-        );
+        $form = $this->createForm(DomainCreateType::class, new DomainCreate(), [
+            'action' => $this->generateUrl('init_submit'),
+            'method' => 'post',
+        ]);
 
-        if ('POST' === $request->getMethod()) {
-            $domainForm->handleRequest($request);
-
-            if ($domainForm->isSubmitted() && $domainForm->isValid()) {
-                $this->creator->create($domain->domain);
-
-                return $this->redirectToRoute('init_user');
-            }
-        }
-
-        return $this->render('Init/domain.html.twig', ['form' => $domainForm->createView()]);
+        return $this->render('Init/domain.html.twig', ['form' => $form->createView()]);
     }
 
-    /**
-     * @param Request $request
-     * @return Response
-     */
-    #[Route(path: '/init/user', name: 'init_user')]
+    #[Route(path: '/init', name: 'init_submit', methods: ['POST'])]
+    public function initSubmit(Request $request): Response
+    {
+        $form = $this->createForm(DomainCreateType::class, new DomainCreate());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->creator->create($form->getData()->domain);
+
+            return $this->redirectToRoute('init_user');
+        }
+
+        return $this->render('Init/domain.html.twig', ['form' => $form->createView()]);
+    }
+
+    #[Route(path: '/init/user', name: 'init_user', methods: ['GET'])]
     public function user(Request $request): Response
     {
         // redirect if already configured
@@ -71,27 +65,28 @@ class InitController extends AbstractController
             return $this->redirectToRoute('index');
         }
 
-        $password = new PlainPassword();
-        $passwordForm = $this->createForm(
-            PlainPasswordType::class,
-            $password,
-            [
-                'action' => $this->generateUrl('init_user'),
-                'method' => 'post',
-            ]
-        );
+        $form = $this->createForm(PlainPasswordType::class, new PlainPassword(), [
+            'action' => $this->generateUrl('init_user'),
+            'method' => 'post',
+        ]);
 
-        if ('POST' === $request->getMethod()) {
-            $passwordForm->handleRequest($request);
+        return $this->render('Init/user.html.twig', ['form' => $form->createView()]);
+    }
 
-            if ($passwordForm->isSubmitted() && $passwordForm->isValid()) {
-                $this->updater->updateAdminPassword($password->getPlainPassword());
-                $request->getSession()->getFlashBag()->add('success', 'flashes.password-change-successful');
+    #[Route(path: '/init/user', name: 'init_user_submit', methods: ['POST'])]
+    public function userSubmit(Request $request): Response
+    {
+        $form = $this->createForm(PlainPasswordType::class, new PlainPassword());
+        $form->handleRequest($request);
 
-                return $this->redirectToRoute('index');
-            }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->updater->updateAdminPassword($form->getData()->getPlainPassword());
+
+            $request->getSession()->getFlashBag()->add('success', 'flashes.password-change-successful');
+
+            return $this->redirectToRoute('index');
         }
 
-        return $this->render('Init/user.html.twig', ['form' => $passwordForm->createView()]);
+        return $this->render('Init/user.html.twig', ['form' => $form->createView()]);
     }
 }
