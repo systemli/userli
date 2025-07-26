990175727fab034e9e7fbf098552418a52092b4d
0x46616c6b <0x46616c6b@users.noreply.github.com>
2025-07-26 22:56:30 +0200

    🔥 Remove the Vagrant special case from the kernel (#835)

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,26 +8,4 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    private const CONFIG_EXTS = '.{php,xml,yaml,yml}';
-
-    public function getCacheDir(): string
-    {
-        $projectDir = parent::getProjectDir();
-        if ('/vagrant' === $projectDir && in_array($this->environment, ['dev', 'test'])) {
-            $projectDir = '/dev/shm/userli';
-        }
-
-        return $projectDir.'/var/cache/'.$this->environment;
-    }
-
-    public function getLogDir(): string
-    {
-        $projectDir = parent::getProjectDir();
-        if ('/vagrant' === $projectDir && in_array($this->environment, ['dev', 'test'])) {
-            $projectDir = '/dev/shm/userli';
-        }
-
-        return $projectDir.'/var/log';
-    }
 }
