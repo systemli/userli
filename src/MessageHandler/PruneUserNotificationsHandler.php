093389219eb1feda646737a3243717a237fdecaf
0x46616c6b <0x46616c6b@users.noreply.github.com>
2025-09-14 12:53:13 +0200

    ♻️ Use Symfony Scheduler for recurring tasks (#882)
    * ♻️ Use Symfony Scheduler for recurring tasks
    
    * ♻️ Use Scheduler for unlink redeemed Vouchers
    
    * 💄 Add UI for Maintenance Tasks
    
    * 🐛 Fix UnlinkRedeemedVouchersHandler

---
+++ src/MessageHandler/PruneUserNotificationsHandler.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MessageHandler;
+
+use DateTimeImmutable;
+use App\Entity\UserNotification;
+use App\Message\PruneUserNotifications;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+final class PruneUserNotificationsHandler
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly LoggerInterface        $logger,
+    )
+    {
+
+    }
+
+    public function __invoke(PruneUserNotifications $message): void
+    {
+        $before = new DateTimeImmutable('-30 days');
+
+        $qb = $this->entityManager->createQueryBuilder()
+            ->delete(UserNotification::class, 'n')
+            ->where('n.creationTime < :before')
+            ->setParameter('before', $before);
+
+        $deleted = $qb->getQuery()->execute();
+
+        $this->logger->info('Pruned user notifications', ['deleted' => $deleted]);
+    }
+}
