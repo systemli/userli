e7aab8b810bdfd51affc2bfaf83a326044404a9e
louis <louis@systemli.org>
2025-11-08 19:06:17 +0100

    ♻️ Remove unnecessary comments and configure method from ReportWeeklyCommand

--- src/Command/ReportWeeklyCommand.php
+++ src/Command/ReportWeeklyCommand.php
@@ -10,19 +10,12 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Class WeeklyReportCommand.
- */
 #[AsCommand(name: 'app:report:weekly', description: 'Send weekly report to all admins')]
 class ReportWeeklyCommand extends Command
 {
     public function __construct(private readonly UserRegistrationInfoHandler $handler)
     {
         parent::__construct();
-    }
-
-    protected function configure(): void
-    {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
