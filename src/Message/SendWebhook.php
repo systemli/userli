247fe4892656188d074a911b87c7fcd0fb8845bd
louis <louis@systemli.org>
2026-01-10 21:34:57 +0100

    ♻️ Use Attributes for Messages

--- src/Message/SendWebhook.php
+++ src/Message/SendWebhook.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Message;
 
+use Symfony\Component\Messenger\Attribute\AsMessage;
+
+#[AsMessage('async')]
 final readonly class SendWebhook
 {
     public function __construct(public string $deliveryId)
