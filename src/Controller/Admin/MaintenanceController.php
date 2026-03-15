227269edc66a89fbf8e4aa5e47d6427216c952a1
yena <yena@systemli.org>
2026-03-15 15:14:33 +0100

    🐛 Validate csrf token in MaintenanceController::run

--- src/Controller/Admin/MaintenanceController.php
+++ src/Controller/Admin/MaintenanceController.php
@@ -11,6 +11,7 @@ use App\Message\RemoveInactiveUsers;
 use App\Message\UnlinkRedeemedVouchers;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
@@ -37,8 +38,12 @@ final class MaintenanceController extends AbstractController
     }
 
     #[Route('/admin/maintenance/run/{task}', name: 'admin_maintenance_run', methods: ['POST'])]
-    public function run(string $task): RedirectResponse
+    public function run(string $task, Request $request): RedirectResponse
     {
+        if (!$this->isCsrfTokenValid('maintenance_'.$task, $request->request->get('_token'))) {
+            return $this->redirectToRoute('admin_maintenance_show');
+        }
+
         $messageClass = self::TASKS[$task] ?? null;
         if (!$messageClass) {
             $this->addFlash('error', 'admin.maintenance.unknown_task');
